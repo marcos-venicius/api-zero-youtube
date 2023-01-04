05e229756fe8fd194d4742d3076108f9aa0a79af
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-01-03 22:48:19 -0300

    init

---
+++ ApiZero/Core/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
